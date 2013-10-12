304599fdf8a5e82f41cf66ac89af4901ff99fea7
kev1shell <kev1shell@neo.tamu.edu>
2013-10-11 23:35:09 -0500

    added create_challenge.php file. beginning implementation of create challenge page

--- UniServer/www/create_challenge.php
+++ UniServer/www/create_challenge.php
@@ -9,4 +9,19 @@
 //includes
 include './function_library.php';
 include './header.php';
+
 ?>
+
+<form action = "send_challenge.php" method="post">
+    First Name: <input name="To" type="text" /> </br>
+    Last Name: <input name="From" type="text" /> </br>
+    Birthday: <input name="Time" type="text" /> </br>
+    Email: <input name="Date" type="DATE" /> </br>
+    Email: <input name="Place" type="text" /> </br>
+    <input name="submit" type="submit" value="Submit"/>
+</form>
+
+<form action = "index.php" method="post">
+    <input name="submit" type="submit" value="Cancel"/>
+
+<?php /*footer*/ include './footer.php'; ?>
---
+++ UniServer/www/send_challenge.php
@@ -0,0 +1,13 @@
+<?php
+
+//send_challenge.php
+//
+//Author: Kevin Shell
+//Date  : 10/11/2013
+//
+
+//handel the new challenge
+
+//go back to index
+header('Location: index.php');
+?>
